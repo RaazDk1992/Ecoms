ce9e12318b3199cf8a99da2c7b15e24ef1175cb5
RaazDk1992 <dhakal1992raaz@gmail.com>
2024-10-07 14:56:48 +0545

    E-commerce App first Commit

---
+++ src/main/java/com/RaazDk/eComs/EComsApplication.java
@@ -0,0 +1,13 @@
+package com.RaazDk.eComs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RaazDk/eComs/EComsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RaazDk.eComs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
